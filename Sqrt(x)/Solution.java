3d32b9b562ef9d462254fd5f889a2a9e2a9d8a0c
QuangMinh <93485271+QuangMinh8699@users.noreply.github.com>
2022-10-31 14:30:23 +0700

    Sqrt(x)

---
+++ Sqrt(x)/Solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int mySqrt(int x) {
+        int answer = (int) Math.sqrt(x);
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        solution.mySqrt(2);
+    }
+}
